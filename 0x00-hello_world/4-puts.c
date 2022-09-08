fb3bdb184ad40b8d9f094a73b4de4776337b2ef2
Your Name <you@example.com>
2022-09-08 13:05:48 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-Serves as the entry point for the program
+ *
+ *Return: default returns zero if no errors else return a non zero value.
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
