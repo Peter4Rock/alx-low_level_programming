a326071dd6c37464e217244917539a838098a98e
Your Name <you@example.com>
2022-09-11 10:06:51 -0700

    Printing Combos

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible combinations of
- *
- * two two-digit
- *
- * The numbers should range from 0 to 99
- *
+
+ * main - program that prints all possible different combinations of
+
+ * three digits
+
  * Numbers must be separated by ,followed by a space
- *
- * All numbers should be printed with two digits. 1 should be printed as 01
- *
+
+ * The two digits must be different
+
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+
  * Return: 0
+
  */
+
+
 
 int main(void)
 {
 int n1 = 48;
-int a = 0;
-int b;
+int n2, n3;
 int com = 44;
-
-while (a <= 99)
+  
+while (n1 <= 57)
 {
-b = a + 1;
-while (b <= 99)
+n2 = n1 + 1;
+while (n2 <= 57)
 {
-putchar((a / 10) + n1);
-putchar((a % 10) + n1);														putchar(32);
-putchar((b / 10) + n1);
-putchar((b % 10) + n1);
-if (a != 98 || b != 99)
+n3 = n2 + 1;
+while (n3 <= 57)
+{
+putchar(n1);
+putchar(n2);
+putchar(n3);
+if (n1 != 55 || n2 != 56 || n3 != 57)
 {
 putchar(com);
 putchar(32);
 }
-b += 1;
+n3 += 1;
 }
-a += 1;
+n2 += 1;
+}
+n1 += 1;
 }
 putchar('\n');
-return (0);
+return (0); 
 }
