a543154ccd95be5b3231008ac585ffac013a7a06
Your Name <you@example.com>
2022-09-13 21:57:41 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: prints the alphabet, in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int i, d;
+for (i = 0; i < 10; i++)
+{
+for (d = 97; d < 123; d++)
+_putchar(d);
+}
+_putchar('\n');
+}
