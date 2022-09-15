2c3e981e68cb3a1a35a2940e43f94b092f1106b2
Your Name <you@example.com>
2022-09-15 12:57:26 -0700

    main.h adjusted

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 
 {
+
   int i;
 
-  printf("Infinite loop incoming :(\n");
 
+  printf("Infinite loop incoming :(\n");
   i = 0;
 
   /* while (i < 10)*/
@@ -21,4 +20,5 @@ int main(void)
     }
   printf("Infinite loop avoided! \\o/\n");
   return (0);
+
 }
