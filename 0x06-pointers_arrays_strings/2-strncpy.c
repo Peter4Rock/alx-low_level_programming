c7f47195fd51dd95205295aa258e52334c9c0f4d
Your Name <you@example.com>
2022-09-22 04:23:09 -0700

    Task- 2_Third commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,31 +18,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 int i;
 
-
-/**
- * src - Functions as a searching sequence
- * iterate through src array
- * where if there is no null byte
- *
- * among the first n bytes of source
- *
- * the string placed in dest will not be
- *
- * null terminated
- */
-
 for (i = 0; i < n && src[i] != '\0'; i++)
 
 dest[i] = src[i];
 
-/**
- * n - Total size of the string to be copied
- * if the length of source is less than n
- *
- * write additional nullbytes to dest to
- *
- * ensure that a total of n bytes is written
- */
 
 while (i < n)
 {
