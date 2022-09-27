6557809dd3f4594b03b42dd142504d0e1f360306
Your Name <you@example.com>
2022-09-27 03:29:22 -0700

    Printing number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - prints an integer
@@ -9,29 +8,18 @@
  *
  * Return: void
  */
-
 void print_number(int n)
-
 {
-
 	unsigned int num;
 
 
-
 	num = n;
-
 	if (n < 0)
-
 	{
-
 		_putchar(45);
-
 		num = -n;
-
 	}
-
 	if (num / 10)
-
 	{
 		print_number(num / 10);
 	}
