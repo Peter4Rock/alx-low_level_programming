579cd51094e4387c61e746011b79648998829857
Your Name <you@example.com>
2022-11-01 22:42:35 -0700

    Elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -143,44 +143,44 @@ void print_version(unsigned char *e_ident)
  * @e_ident: A pointer to an array containing the ELF version.
  */
 
-void print_osabi(unsigned char *e_ident)
+void print_os/abi(unsigned char *e_ident)
 {
 	printf("  OS/ABI:                            ");
 
-	switch (e_ident[EI_OSABI])
+	switch (e_ident[EI_OS/ABI])
 	{
-	case ELFOSABI_NONE:
+	case ELFOS/ABI_NONE:
 		printf("UNIX - System V\n");
 		break;
-	case ELFOSABI_HPUX:
+	case ELFOS/ABI_HPUX:
 		printf("UNIX - HP-UX\n");
 		break;
-	case ELFOSABI_NETBSD:
+	case ELFOS/ABI_NETBSD:
 		printf("UNIX - NetBSD\n");
 		break;
-	case ELFOSABI_LINUX:
+	case ELFOS/ABI_LINUX:
 		printf("UNIX - Linux\n");
 		break;
-	case ELFOSABI_SOLARIS:
+	case ELFOS/ABI_SOLARIS:
 		printf("UNIX - Solaris\n");
 		break;
-	case ELFOSABI_IRIX:
+	case ELFOS/ABI_IRIX:
 		printf("UNIX - IRIX\n");
 		break;
-	case ELFOSABI_FREEBSD:
+	case ELFOS/ABI_FREEBSD:
 		printf("UNIX - FreeBSD\n");
 		break;
-	case ELFOSABI_TRU64:
+	case ELFOS/ABI_TRU64:
 		printf("UNIX - TRU64\n");
 		break;
-	case ELFOSABI_ARM:
+	case ELFOS/ABI_ARM:
 		printf("ARM\n");
 		break;
-	case ELFOSABI_STANDALONE:
+	case ELFOS/ABI_STANDALONE:
 		printf("Standalone App\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+		printf("<unknown: %x>\n", e_ident[EI_OS/ABI]);
 	}
 }
 
